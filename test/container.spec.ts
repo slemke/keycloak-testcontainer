59c1e036fe545325c62fd8e29845c14f94abfce5
Sascha Lemke <sascha-lemke@gmx.de>
2023-08-09 22:22:12 +0200

    run keycloak container with exposed port in tests

--- test/container.spec.ts
+++ test/container.spec.ts
@@ -5,8 +5,11 @@ describe('Container', () => {
 	let startedContainer: StartedKeycloakContainer;
 
 	it('should start new keycloak container', async () => {
-			startedContainer = await new KeycloakContainer().start();
+		startedContainer = await new KeycloakContainer()
+			.withExposedPorts(8080)
+			.start();
 	});
+
 
 	it('should stop keycloak container', async () => {
 		await startedContainer.stop();
