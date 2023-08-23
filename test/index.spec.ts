dc9d43c7353e12fb1efe03b9738e8dfcc10a5e8c
Sascha Lemke <sascha-lemke@gmx.de>
2023-08-23 07:56:05 +0200

    add test to verify import and container start

---
+++ test/index.spec.ts
@@ -0,0 +1,9 @@
+import { KeycloakContainer } from "../src";
+
+describe('Import', () => {
+    it('should be able to import and start keycloak container', async () => {
+        const container = new KeycloakContainer();
+        const startedContainer = await container.start();
+        await startedContainer.stop();
+    });
+})
