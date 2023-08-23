00fa3f03475022af5abe2fc93337aa74d3ea2dc0
Sascha Lemke <sascha-lemke@gmx.de>
2023-08-23 07:37:13 +0200

    remove default export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { KeycloakContainer } from './container/container';
 import { StartedKeycloakContainer } from 'container/started-container';
 
-export default new KeycloakContainer();
-
 export { KeycloakContainer };
-
 export { StartedKeycloakContainer };
