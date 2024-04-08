886c1dc2b44b211c2e3594f26d314d76df2605c2
Sascha Lemke <sascha-lemke@gmx.de>
2024-04-08 12:44:16 +0200

    add keycloak admin client

--- src/configuration/environment.ts
+++ src/configuration/environment.ts
@@ -1,14 +1,18 @@
+import { Keycloak } from '../keycloak.js';
+
 export interface AdminUser {
 	username: string;
 	password: string;
 }
 
+export const defaultAdminUser = {
+	username: Keycloak.ADMIN_USER,
+	password: Keycloak.ADMIN_PASSWORD
+};
+
 export class EnvironmentBuilder {
 
-	private adminUser: AdminUser = {
-		username: 'admin',
-		password: 'admin'
-	};
+	private adminUser: AdminUser = defaultAdminUser;
 
 	private hostname: string | undefined;
 
@@ -21,7 +25,7 @@ export class EnvironmentBuilder {
 	}
 
 	public build(): { [key: string]: string } {
-		const { username, password } = this.adminUser;		
+		const { username, password } = this.adminUser;
 		return {
 			KEYCLOAK_ADMIN: username,
 			KEYCLOAK_ADMIN_PASSWORD: password,
